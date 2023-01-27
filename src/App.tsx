4952ade67bb0327ff6788d15e01e7332c603dc65
NiceTwice <onlynicetwice@gmail.com>
2023-01-27 20:00:44 -0300

    pokemon api

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,22 @@
 import React, { useEffect, useState } from 'react';
-
-interface Sub {
-  nick:string,
-  subMonths:number,
-  avatar:string,
-  description?:string
-}
-
-const initial_state = [
-    {
-      nick:"dapelu",
-      subMonths:4,
-      avatar:"https://i.pravatar.cc/150?u=dapelu",
-      description:"dapelu es buena onda"
-    },
-    {
-      nick:"midu",
-      subMonths:4,
-      avatar:"https://i.pravatar.cc/150?u=midudev"
-    },
-    {
-      nick:"Marc",
-      subMonths:4,
-      avatar:"https://i.pravatar.cc/150?u=marc"
-    }
-  ]
+import axios from "axios"
 
 function App() {
   
-  const [subs, setSubs] = useState<Sub[]>([])
+  const [pokemons, setPokemons] = useState<Object[]>([])
 
   useEffect(()=>{
-    setSubs(initial_state)
-  },[])
 
+  },[])
+console.log(pokemons)
   return (
    <div className='flex flex-col justify-center items-center h-screen w-full bg-slate-700'>
       <h1 className='text-4xl font-serif text-white font-semibold'>Subs</h1>
       <div className='gap-4 grid grid-cols-1 lg:grid-cols-3 px-10 py-4 mt-10 border border-white'>
         {
-          subs.map(sub=>(
+          pokemons?.map(pokemon => (
             <div className='text-white text-center'>
-              <img alt="img" src={sub.avatar} />
-              <h3>{sub.nick}</h3>
+
             </div>
           ))
         }
---
+++ src/components/PokemonItem.tsx
@@ -0,0 +1,11 @@
+const PokemonItem = () => {
+    return ( 
+        <>
+            <div>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default PokemonItem;
