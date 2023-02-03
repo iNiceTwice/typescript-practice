c68ac84ec3c38dbb7ddc958f75b8396032ed8734
NiceTwice <onlynicetwice@gmail.com>
2023-02-03 17:54:04 -0300

    adding types & characters items

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios"
+import { Character } from './types';
 import { getCharacters } from './api/getCharacters';
+import CharacterItem from './components/CharacterItem';
 
 function App():JSX.Element {
   
-  const [chars, setChars ] = useState<Object[]>([])
+  const [chars, setChars ] = useState<Character[]>([])
 
-  const getData = async () => {
+  const getData = async ():Promise<void> => {
     let data = await getCharacters()
     setChars(data)
   }
@@ -14,12 +15,25 @@ function App():JSX.Element {
   useEffect(()=>{
    getData()
   },[])
+
   console.log(chars)
   return (
-   <div className='flex flex-col justify-center items-center h-screen w-full bg-slate-700'>
+   <div className='flex flex-col justify-center items-center h-full w-full bg-slate-700'>
       <h1 className='text-4xl font-serif text-white font-semibold'>Personjes</h1>
-      <div className='gap-4 grid grid-cols-1 lg:grid-cols-3 px-10 py-4 mt-10 border border-white'>
-
+      <div className='gap-4 grid grid-cols-1 lg:grid-cols-3 px-10 py-4 mt-10 w-9/12'>
+        {
+          chars?.map((char) => (
+            <div key={char.id}>
+              <CharacterItem
+                id={char.id}
+                name={char.name}
+                gender={char.gender}
+                status={char.status}
+                image={char.image}
+             />
+            </div>
+          ))
+        }
       </div>
    </div>
   );
---
+++ src/components/CharacterItem.tsx
@@ -0,0 +1,26 @@
+import { Character } from "../types";
+
+const CharacterItem = ({ id,name,gender,image,status }:Character):JSX.Element => {
+    return ( 
+        <>
+            <div className="h-full p-4 rounded-lg bg-white flex gap-x-6">
+                <div>
+                    <img className="rounded-full shadow-md" src={ image } width={100}/>
+                </div>
+                <div className="w-full">
+                    <div className="flex items-center">
+                        <h3 className="text-xl font-semibold">{ name }</h3>
+                        { status === "Alive" ? <span className="p-1 bg-green-600 rounded-full ml-4 h-fit"></span> : null }
+                        { status === "Dead" ? <span className="p-1 bg-red-600 rounded-full ml-4 h-fit"></span> : null }
+                        { status === "unknown" ? <span className="p-1 bg-gray-600 rounded-full ml-4 h-fit"></span> : null }
+                    </div>
+                    <div>
+                        {gender}
+                    </div>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default CharacterItem;
